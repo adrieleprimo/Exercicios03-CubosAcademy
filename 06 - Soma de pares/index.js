e232c57339b2e58ceb7db26ffc51cc57cf7be01a
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-22 11:27:17 -0300

    Soma de pares realizado

--- 06 - Soma de pares/index.js
+++ 06 - Soma de pares/index.js
@@ -1,2 +1,12 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
-
+let pares =[];
+for(numero of numeros){
+    if(numero%2===0){
+        pares.push(numero);
+    }
+}
+let soma = 0;
+for(par of pares){
+    soma+=par;
+}
+console.log(soma);
