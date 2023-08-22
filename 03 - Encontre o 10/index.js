834a91171976b2ba9d737834ed05d1025b714793
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-22 10:43:57 -0300

    Numero encontrado na array

--- 03 - Encontre o 10/index.js
+++ 03 - Encontre o 10/index.js
@@ -1,2 +1,6 @@
-const numeros = [54, 22, 14, 87, 10, 284];
-
+const numeros = [54, 22, 14, 10, 87, 284];
+let encontrar = 0;
+for(numero of numeros){
+    encontrar = numeros.indexOf(10);
+}
+console.log(encontrar);
