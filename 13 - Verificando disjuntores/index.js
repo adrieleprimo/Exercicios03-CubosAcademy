dd7f22dbdee4d3447539c8a2f507208891a24785
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-22 14:37:10 -0300

    Verificação de disjuntores realizada

--- 13 - Verificando disjuntores/index.js
+++ 13 - Verificando disjuntores/index.js
@@ -1,2 +1,14 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+let posicao = [];
+
+for (let i =0; i<disjuntores.length;i++){
+    if (disjuntores[i]===true){
+         posicao.push(i);    
+    }
+}  
+for (i of posicao){
+    console.log(i);
+}
+
+
