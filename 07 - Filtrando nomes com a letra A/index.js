a1ace0edf0d2f6117cc51d09a83c5e0b8b99257f
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-22 11:56:28 -0300

    Nomes filtrados

--- 07 - Filtrando nomes com a letra A/index.js
+++ 07 - Filtrando nomes com a letra A/index.js
@@ -1,1 +1,9 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+let nomesComA = [];
+let encontrar = 0;
+for(nome of nomes){
+    if(nome[encontrar] ==="A" || nome[encontrar] ==="a"){
+        nomesComA.push(nome);
+    }
+}
+console.log(nomesComA);
