e9fb61cd54b55e1c5b8e2049dc3e82dbd3a93113
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-22 13:09:40 -0300

    Numeros filtrados

--- 11 - Filtrando numeros/index.js
+++ 11 - Filtrando numeros/index.js
@@ -1,2 +1,9 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 10, 13, 17, 21, 26, 34, 100, 118, 245];
 
+let novosOriginais = [];
+for(numero of original){
+    if(numero >= 10 && numero <=20 || numero>100){
+        novosOriginais.push(numero);
+    }
+}
+console.log(novosOriginais);
