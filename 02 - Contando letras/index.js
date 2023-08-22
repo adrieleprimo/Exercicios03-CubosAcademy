a28aa62cbfc5799a510ffa17122a1cfb7535d477
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-22 09:52:03 -0300

    Contagem de letras realizada

--- 02 - Contando letras/index.js
+++ 02 - Contando letras/index.js
@@ -1,1 +1,14 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+let somaLetras = 0;
+let encontrado = false;
+for(letra of letras){
+    if (letra === "E" || letra === "e"){
+        somaLetras++;
+        encontrado = true;
+    }
+}
+if(encontrado === true){
+    console.log(`Foram encontradas ${somaLetras} letras "E" ou "e"`);
+} else {
+    console.log("Nenhuma letra 'E' ou 'e' foi encontrada.");
+}
